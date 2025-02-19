e58b50bcfc3c13e68afa361afbf0af25b92ed032
Redical23 <gfhkhihhi@gmail.com>
2025-02-20 00:45:54 +0530

    version1.61

--- src/app/lib/dbConnect.js
+++ src/app/lib/dbConnect.js
@@ -1,13 +1,40 @@
 // db.js
 import mongoose from "mongoose";
 
-const dbconnect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
-  }
-};
+const MONGODB_URI = process.env.MONGODB_URI;
 
-export default dbconnect;
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads in development.
+ * Do not import this file in client-side code.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  // If already connected, return the cached connection
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  // If a connection promise is not already stored, create one
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Additional options can be added here
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
