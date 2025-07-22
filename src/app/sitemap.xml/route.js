ece0939ce33bba993d9894c91e48df42c286db12
Redical23 <gfhkhihhi@gmail.com>
2025-07-22 11:00:33 +0530

    version1.04.146

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,14 +1,16 @@
+// src/app/sitemap.xml/route.js
+
 export async function GET() {
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-      <loc>https://kanoonikarwayahi.com/</loc>
+      <loc>https://kanoonikarwayahi.com</loc>
       <changefreq>weekly</changefreq>
       <priority>1.0</priority>
     </url>
   </urlset>`;
 
-  return new Response(sitemap, {
+  return new Response(body, {
     headers: {
       'Content-Type': 'application/xml',
     },
